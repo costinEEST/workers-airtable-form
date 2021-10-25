f49a45640e0c689c069fbdff2b863344033296b6
Costi <mail.la.costi@gmail.com>
2021-10-25 22:58:45 +0300

    fix the HTML warnings

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,34 @@
-import { PencilAltIcon } from '@heroicons/react/outline'
+import { PencilAltIcon } from "@heroicons/react/outline";
 
-import Form from './Form'
+import Form from "./Form";
 
-const REPO_URL = "https://github.com/signalnerve/workers-airtable-form"
+const REPO_URL = "https://github.com/signalnerve/workers-airtable-form";
 
-const ExternalLink = ({ children, hoverColor = "text-teal-800", href }) =>
+const ExternalLink = ({ children, hoverColor = "text-teal-800", href }) => (
   <a
     className={`underline hover:${hoverColor} transition-all`}
     href={href}
     target="_blank"
+    rel="noreferrer noopener"
   >
     {children}
   </a>
+);
 
 export default function FormPage() {
   return (
     <div className="min-h-screen bg-white">
       <div className="bg-warm-gray-50">
         <nav
-          className="relative max-w-7xl mx-auto flex items-center justify-between pt-6 px-6 xl:px-8"
+          className="relative flex items-center justify-between px-6 pt-6 mx-auto max-w-7xl xl:px-8"
           aria-label="Global"
         >
           <div className="flex items-center flex-1">
             <div className="flex items-center justify-between w-full lg:w-auto">
-              <a href="#">
+              <button>
                 <span className="sr-only">Workflow</span>
-                <PencilAltIcon
-                  className="text-teal-800 h-8 w-auto sm:h-10"
-                />
-              </a>
+                <PencilAltIcon className="w-auto h-8 text-teal-800 sm:h-10" />
+              </button>
             </div>
           </div>
         </nav>
@@ -38,12 +38,20 @@ export default function FormPage() {
         {/* Header */}
         <div className="bg-warm-gray-50">
           <div className="py-24 lg:py-32">
-            <div className="relative z-10 max-w-7xl mx-auto pl-4 pr-8 sm:px-6 lg:px-8">
+            <div className="relative z-10 pl-4 pr-8 mx-auto max-w-7xl sm:px-6 lg:px-8">
               <h1 className="text-4xl font-extrabold tracking-tight text-warm-gray-900 sm:text-5xl lg:text-6xl">
                 Workers Airtable Form Example
               </h1>
-              <p className="mt-6 text-xl text-warm-gray-500 max-w-3xl">
-                An example form that uses serverless functions, provided by <ExternalLink href="https://workers.dev">Cloudflare Workers</ExternalLink>, to submit form data to an <ExternalLink href="https://airtable.com">Airtable</ExternalLink> table.{" "}
+              <p className="max-w-3xl mt-6 text-xl text-warm-gray-500">
+                An example form that uses serverless functions, provided by{" "}
+                <ExternalLink href="https://workers.dev">
+                  Cloudflare Workers
+                </ExternalLink>
+                , to submit form data to an{" "}
+                <ExternalLink href="https://airtable.com">
+                  Airtable
+                </ExternalLink>{" "}
+                table.{" "}
                 <ExternalLink href={REPO_URL}>
                   Check out the source to learn more.
                 </ExternalLink>
@@ -53,12 +61,18 @@ export default function FormPage() {
         </div>
 
         {/* Contact section */}
-        <section className="relative bg-white pb-24" aria-labelledby="contactHeading">
-          <div className="absolute w-full h-1/2 bg-warm-gray-50" aria-hidden="true" />
+        <section
+          className="relative pb-24 bg-white"
+          aria-labelledby="contactHeading"
+        >
+          <div
+            className="absolute w-full h-1/2 bg-warm-gray-50"
+            aria-hidden="true"
+          />
           {/* Decorative dot pattern */}
-          <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <svg
-              className="absolute z-0 top-0 right-0 transform -translate-y-16 translate-x-1/2 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
+              className="absolute top-0 right-0 z-0 transform translate-x-1/2 -translate-y-16 sm:translate-x-1/4 md:-translate-y-24 lg:-translate-y-72"
               width={404}
               height={384}
               fill="none"
@@ -74,13 +88,24 @@ export default function FormPage() {
                   height={20}
                   patternUnits="userSpaceOnUse"
                 >
-                  <rect x={0} y={0} width={4} height={4} className="text-warm-gray-200" fill="currentColor" />
+                  <rect
+                    x={0}
+                    y={0}
+                    width={4}
+                    height={4}
+                    className="text-warm-gray-200"
+                    fill="currentColor"
+                  />
                 </pattern>
               </defs>
-              <rect width={404} height={384} fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)" />
+              <rect
+                width={404}
+                height={384}
+                fill="url(#64e643ad-2176-4f86-b3d7-f2c5da3b6a6d)"
+              />
             </svg>
           </div>
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div className="relative bg-white shadow-xl">
               <h2 id="contactHeading" className="sr-only">
                 Contact us
@@ -88,9 +113,12 @@ export default function FormPage() {
 
               <div className="grid grid-cols-1 lg:grid-cols-3">
                 {/* Contact information */}
-                <div className="relative overflow-hidden py-10 px-6 bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:p-12">
+                <div className="relative px-6 py-10 overflow-hidden bg-gradient-to-b from-teal-500 to-teal-600 sm:px-10 xl:p-12">
                   {/* Decorative angle backgrounds */}
-                  <div className="absolute inset-0 pointer-events-none sm:hidden" aria-hidden="true">
+                  <div
+                    className="absolute inset-0 pointer-events-none sm:hidden"
+                    aria-hidden="true"
+                  >
                     <svg
                       className="absolute inset-0 w-full h-full"
                       width={343}
@@ -121,7 +149,7 @@ export default function FormPage() {
                     </svg>
                   </div>
                   <div
-                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none sm:block lg:hidden"
+                    className="absolute top-0 bottom-0 right-0 hidden w-1/2 pointer-events-none sm:block lg:hidden"
                     aria-hidden="true"
                   >
                     <svg
@@ -154,7 +182,7 @@ export default function FormPage() {
                     </svg>
                   </div>
                   <div
-                    className="hidden absolute top-0 right-0 bottom-0 w-1/2 pointer-events-none lg:block"
+                    className="absolute top-0 bottom-0 right-0 hidden w-1/2 pointer-events-none lg:block"
                     aria-hidden="true"
                   >
                     <svg
@@ -186,17 +214,29 @@ export default function FormPage() {
                       </defs>
                     </svg>
                   </div>
-                  <h3 className="text-lg font-medium text-white">Example form</h3>
-                  <p className="mt-6 text-base text-teal-50 max-w-3xl">
-                    A fully-featured HTML5 <code>form</code> element that submits data to a serverless function.
-                    This data is added to an <ExternalLink hoverColor="text-teal-300" href="https://airtable.com">Airtable</ExternalLink> table, which can be connected with various integrations and extensions to notify
-                    you on new submissions and changes.
+                  <h3 className="text-lg font-medium text-white">
+                    Example form
+                  </h3>
+                  <p className="max-w-3xl mt-6 text-base text-teal-50">
+                    A fully-featured HTML5 <code>form</code> element that
+                    submits data to a serverless function. This data is added to
+                    an{" "}
+                    <ExternalLink
+                      hoverColor="text-teal-300"
+                      href="https://airtable.com"
+                    >
+                      Airtable
+                    </ExternalLink>{" "}
+                    table, which can be connected with various integrations and
+                    extensions to notify you on new submissions and changes.
                   </p>
                 </div>
 
                 {/* Contact form */}
-                <div className="py-10 px-6 sm:px-10 lg:col-span-2 xl:p-12">
-                  <h3 className="text-lg font-medium text-warm-gray-900">Send us a message</h3>
+                <div className="px-6 py-10 sm:px-10 lg:col-span-2 xl:p-12">
+                  <h3 className="text-lg font-medium text-warm-gray-900">
+                    Send us a message
+                  </h3>
                   <Form />
                 </div>
               </div>
@@ -209,12 +249,16 @@ export default function FormPage() {
         <h2 id="footerHeading" className="sr-only">
           Footer
         </h2>
-        <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8">
+        <div className="px-4 py-12 mx-auto max-w-7xl sm:px-6 lg:py-16 lg:px-8">
           <p className="text-base text-warm-gray-400 xl:text-center">
-            Workers Airtable Form Example is <ExternalLink hoverColor="text-warm-gray-200" href={REPO_URL}>open source on GitHub</ExternalLink>.
+            Workers Airtable Form Example is{" "}
+            <ExternalLink hoverColor="text-warm-gray-200" href={REPO_URL}>
+              open source on GitHub
+            </ExternalLink>
+            .
           </p>
         </div>
       </footer>
     </div>
-  )
+  );
 }
